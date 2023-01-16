f3ac75ccded6aac12706e6cbe183d1f4e9b9b105
Yaroslav Chelombitko <57654715+AragonerUA@users.noreply.github.com>
2023-01-16 14:06:32 +0200

    Delete Topics directory

--- Topics/Handling missing values/Replace with the mode/main.py
+++
@@ -1,9 +0,0 @@
-#  write your code here 
-import pandas as pd
-
-
-if __name__ == "__main__":
-    df = pd.read_csv("/Users/aragonerua/PycharmProjects/Data Analysis for Hospitals/Topics/Handling missing values/Replace with the mode/data/dataset/input.txt", sep=",")
-    modes = df["location"].mode()[0]
-    df["location"].fillna(modes, inplace=True)
-    print(df.head(5))
